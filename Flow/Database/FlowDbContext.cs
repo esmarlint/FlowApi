875f8354caba59fd61af8b2d6a413cbe3ab0dca7
Nelson Esmarlin Rosario Rodriguez <esmarlin9@gmail.com>
2020-09-05 18:19:41 -0400

    To add context for the application.

---
+++ Flow/Database/FlowDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Flow.Auth;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Flow.Database
+{
+    public class FlowDbContext:IdentityDbContext<ApplicationUser>
+    {
+        public FlowDbContext(DbContextOptions<FlowDbContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
